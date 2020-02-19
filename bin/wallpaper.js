2d746ab14f3320f404e0c492c526f4cb515c8395
Scott Buchanan <scott.buchanan@medavie.bluecross.ca>
2020-02-19 16:47:09 -0400

    initial commit with code copied

---
+++ bin/wallpaper.js
@@ -0,0 +1,149 @@
+#!/usr/bin/env node
+
+const program = require("commander");
+const wallpaper = require("wallpaper");
+const isUrl = require("is-url-superb");
+const {
+  setWallpaper,
+  switchWallpaper,
+  logError,
+  logSuccess,
+  clearWallpapers,
+  clearCurrentWallpaper
+} = require("../lib/util");
+const Unsplash = require("../lib/unsplash");
+const Bing = require("../lib/bing");
+const QJP = require("../lib/qvjunping");
+const NetBian = require("../lib/netbian");
+
+const { version } = require("../package.json");
+const bingApi = new Bing();
+const qjpApi = new QJP();
+const unsplashApi = new Unsplash();
+const netbianApi = new NetBian();
+
+program.version(version);
+
+program
+  .command("update <file>")
+  .description("Update desktop wallpaper with file path or remote url")
+  .option(
+    "-s --scale [mode]",
+    "Scaling method: [auto, fill, fit, stretch, center](Default: auto) Only available on macOS"
+  )
+  .action(async (file, { scale }) => {
+    await setWallpaper(file, scale);
+  });
+
+program
+  .command("get")
+  .description("Get desktop wallpaper real path")
+  .action(async () => {
+    logSuccess(
+      `Your current desktop wallpaper is from: ${await wallpaper.get()}`
+    );
+  });
+
+program
+  .command("random <from>")
+  .description("Random desktop wallpaper change. from: [QJP, unsplash, bing]")
+  .option(
+    "-s --scale [mode]",
+    "Scaling method: [auto, fill, fit, stretch, center](Default: auto) Only available on macOS"
+  )
+  .action(async (from, { scale }) => {
+    let url = "";
+    from = from.toUpperCase();
+
+    if (from === "QJP") {
+      url = await qjpApi.getRandom();
+    } else if (from === "UNSPLASH") {
+      url = await unsplashApi.getRandom();
+    } else if (from === "BING") {
+      url = await bingApi.getRandom();
+    } else if (from === "NETBIAN") {
+      url = await netbianApi.getRandom();
+    } else {
+      return logError(`unknown argument '${from}'. See 'wallpaper random -h'`);
+    }
+
+    if (!isUrl(url)) {
+      return logError("fetch random source failed, retry it!");
+    }
+
+    await setWallpaper(url, scale);
+  });
+
+program
+  .command("daily <from>")
+  .description("Daily wallpaper. from: [bing, unsplash, netbian]")
+  .option(
+    "-s --scale [mode]",
+    "Scaling method: [auto, fill, fit, stretch, center](Default: auto) Only available on macOS"
+  )
+  .action(async (from, { scale }) => {
+    let url = "";
+    from = from.toUpperCase();
+
+    if (from === "BING") {
+      url = await bingApi.getDaily();
+    } else if (from === "UNSPLASH") {
+      url = await unsplashApi.getDaily();
+    } else {
+      return logError(`unknown argument ${from}. See 'wallpaper daily -h'`);
+    }
+
+    if (!isUrl(url)) {
+      return logError("fetch random source failed, retry it!");
+    }
+
+    await setWallpaper(url, scale);
+  });
+
+program
+  .command("clear")
+  .description("Clear the currently used wallpaper")
+  .option("-a --all", "Clear all the cached wallpapers")
+  .action(async ({ all }) => {
+    if (all) {
+      await clearWallpapers();
+    } else {
+      await clearCurrentWallpaper();
+    }
+  });
+
+program
+  .command("switch")
+  .description("Switch cached wallpapers")
+  .option("-p --pre", "Switch the previous wallpaper")
+  .option("-n --next", "Switch the next wallpaper")
+  .option("-l --latest", "Switch the latest wallpaper")
+  .action(async ({ pre, next, latest }) => {
+    await switchWallpaper({ pre, next, latest });
+  });
+
+program.on("--help", function() {
+  console.log("");
+  console.log("Examples:");
+  console.log("  $ wallpaper update https://examples.com/wallpaper.jpg");
+  console.log("  $ wallpaper random QJP");
+  console.log("  $ wallpaper daily bing");
+});
+
+program.on("command:*", function() {
+  logError(
+    `Invalid command: ${program.args.join(
+      " "
+    )}\nSee --help for a list of available commands.`
+  );
+  process.exit(1);
+});
+
+Object.getPrototypeOf(program).constructor.prototype.unknownOption = flags => {
+  logError(
+    `unknown option: ${flags}\nSee --help for a list of available commands.`
+  );
+  process.exit(1);
+};
+
+program.parse(process.argv);
---
+++ test/test.js
@@ -0,0 +1,210 @@
+const test = require("ava");
+const execa = require("execa");
+const os = require("os");
+const path = require("path");
+const fs = require("fs-extra");
+
+const bin = "../bin/wallpaper.js";
+const _cache = path.join(process.env.HOME, ".wallpaper", ".cache.json");
+let cacheBefore = "";
+const temp = path.join(__dirname, "./test_images/test3.jpg");
+
+test.before(async t => {
+  try {
+    cacheBefore = await fs.readFile(_cache, "utf8");
+  } catch (error) {
+    // console.log(error);
+  }
+});
+
+test.beforeEach(async t => {
+  await execa.stdout(bin, ["update", temp], {
+    cwd: __dirname
+  });
+});
+
+test.after(async t => {
+  if (!!cacheBefore) {
+    await fs.writeFile(_cache, cacheBefore);
+    await execa.stdout(bin, ["switch", "-l"], {
+      cwd: __dirname
+    });
+  } else {
+    await fs.unlink(_cache);
+  }
+});
+
+test.serial("get", async t => {
+  t.true((await execa.stdout(bin, ["get"], { cwd: __dirname })).length > 0);
+});
+
+test.serial("switch", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  const testImagsPaths = [
+    path.join(__dirname, "./test_images/test1.jpg"),
+    path.join(__dirname, "./test_images/test2.jpg")
+  ];
+
+  await execa.stdout(bin, ["update", testImagsPaths[0]], {
+    cwd: __dirname
+  });
+  await execa.stdout(bin, ["update", testImagsPaths[1]], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["switch", "-p"], {
+    cwd: __dirname
+  });
+  const preImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    preImagePath.includes(path.resolve(testImagsPaths[0])) &&
+      !preImagePath.includes(orignalImagePath)
+  );
+
+  await execa.stdout(bin, ["switch", "-n"], {
+    cwd: __dirname
+  });
+  const nextImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    nextImagePath.includes(path.resolve(testImagsPaths[1])) &&
+      !nextImagePath.includes(preImagePath)
+  );
+
+  await execa.stdout(bin, ["switch", "-l"], {
+    cwd: __dirname
+  });
+  t.is(await execa.stdout(bin, ["get"], { cwd: __dirname }), nextImagePath);
+});
+
+test.serial("update", async t => {
+  const tempImagePath = path.join(__dirname, "./test_images/test1.jpg");
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["update", tempImagePath], {
+    cwd: __dirname
+  });
+
+  const updateImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    updateImagePath.includes(path.resolve(tempImagePath)) &&
+      !updateImagePath.includes(orignalImagePath)
+  );
+});
+
+test.serial("daily:bing", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["daily", "bing"], {
+    cwd: __dirname
+  });
+
+  const randomImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    randomImagePath.includes(os.tmpdir()) &&
+      !randomImagePath.includes(orignalImagePath)
+  );
+});
+
+test.serial("daily:unsplash", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["daily", "unsplash"], {
+    cwd: __dirname
+  });
+
+  const randomImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    randomImagePath.includes(os.tmpdir()) &&
+      !randomImagePath.includes(orignalImagePath)
+  );
+});
+
+test.serial("random:QJP", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["random", "QJP"], {
+    cwd: __dirname
+  });
+
+  const randomImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    randomImagePath.includes(os.tmpdir()) &&
+      !randomImagePath.includes(orignalImagePath)
+  );
+});
+
+test.serial("random:unsplash", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["random", "unsplash"], {
+    cwd: __dirname
+  });
+
+  const randomImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    randomImagePath.includes(os.tmpdir()) &&
+      !randomImagePath.includes(orignalImagePath)
+  );
+});
+
+test.serial("random:bing", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["random", "bing"], {
+    cwd: __dirname
+  });
+
+  const randomImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    randomImagePath.includes(os.tmpdir()) &&
+      !randomImagePath.includes(orignalImagePath)
+  );
+});
+
+test.serial("random:netbian", async t => {
+  const orignalImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+
+  await execa.stdout(bin, ["random", "netbian"], {
+    cwd: __dirname
+  });
+
+  const randomImagePath = await execa.stdout(bin, ["get"], {
+    cwd: __dirname
+  });
+  t.true(
+    randomImagePath.includes(os.tmpdir()) &&
+      !randomImagePath.includes(orignalImagePath)
+  );
+});
